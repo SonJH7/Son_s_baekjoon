14f9eb49d6152be1d2681e58f181f66ae4ede882
SonJH7 <129269335+SonJH7@users.noreply.github.com>
2024-11-22 22:24:48 +0900

    [level 1] Title: 모의고사, Time: 0.30 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,29 +1,25 @@
+
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
+
 using namespace std;
+
+vector<int> one = {1,2,3,4,5};
+vector<int> two = {2,1,2,3,2,4,2,5};
+vector<int> thr = {3,3,1,1,2,2,4,4,5,5};
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    vector<int> s1 = {1, 2, 3, 4, 5};
-    vector<int> s2 = {2, 1, 2, 3, 2, 4, 2, 5};
-    vector<int> s3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-    
-    int c1 = 0, c2 = 0, c3 = 0;
-    
-    for (int i = 0; i < answers.size(); i++) {
-        if (answers[i] == s1[i % s1.size()]) c1++;
-        if (answers[i] == s2[i % s2.size()]) c2++;
-        if (answers[i] == s3[i % s3.size()]) c3++;
+    vector<int> they(3);
+    for(int i=0; i<answers.size(); i++) {
+        if(answers[i] == one[i%one.size()]) they[0]++;
+        if(answers[i] == two[i%two.size()]) they[1]++;
+        if(answers[i] == thr[i%thr.size()]) they[2]++;
     }
-    
-    // 가장 높은 점수를 찾기
-    int max_score = max({c1, c2, c3});
-    
-    if (c1 == max_score) answer.push_back(1);
-    if (c2 == max_score) answer.push_back(2);
-    if (c3 == max_score) answer.push_back(3);
-    
+    int they_max = *max_element(they.begin(),they.end());
+    for(int i = 0; i< 3; i++) {
+        if(they[i] == they_max) answer.push_back(i+1);
+    }
     return answer;
 }
